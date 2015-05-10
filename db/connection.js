0e8924f034b4c2882b768208d044def2f367af8f
samutamm <samutamm@gmail.com>
2015-05-10 22:21:09 +0300

    Deploy to heroku

--- db/connection.js
+++ db/connection.js
@@ -1,8 +1,8 @@
 var Sequelize = require('sequelize');
 
-var sequelize = new Sequelize('foorumi', '', '', {
-  dialect: 'sqlite',
-  storage: 'db/database.sqlite'
+var sequelize = new Sequelize('postgres://akvqxdmipxutfp:UXzEs0uKkoPZAhSPaa4p_WmT3S@ec2-184-73-253-4.compute-1.amazonaws.com:5432/d4gpabuehh8q2a', {
+  dialect: 'postgres',
+  protocol: 'postgres'
 });
 
 module.exports = {
